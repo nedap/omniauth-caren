ca4385422b13cff22b26bb89d342144c8f560187
Stefan Vermaas <stefanvermaas1991@gmail.com>
2016-09-06 22:26:11 +0200

    Fixes a bug introduced in 1.4.0 of omniauth-oauth2
    For more information, see:
    https://github.com/intridea/omniauth-oauth2/issues/81
    
    This change in the codebase fixes the problems and removes the hard
    version dependecy of version 1.3.1 of the omniauth-oauth2 gem.

--- lib/omniauth/strategies/caren.rb
+++ lib/omniauth/strategies/caren.rb
@@ -14,8 +14,11 @@ module OmniAuth
 
       option :authorize_options, [:scope, :display, :auth_type]
 
+      # Define the uid of the resource
       uid { person["id"] }
 
+      # Defines the information we want to expose to the omniauth client
+      # @return [Hash] the user information of Caren
       info do
         {
           :first_name     => person["first_name"],
@@ -33,10 +36,14 @@ module OmniAuth
         skip_info? ? {} : { :raw_info => person }
       end
 
+      # Retrieves the user information from the Caren Api
+      # @return [Hash] the parsed json response for the user object
       def raw_info
         @raw_info ||= access_token.get('/api/v1/user.json').parsed
       end
 
+      # Retrieves the embedded person object from the `raw_info`
+      # @return [Hash] the personal information of the caren account
       def person
         raw_info["_embedded"]["person"]
       end
@@ -56,6 +63,14 @@ module OmniAuth
 
           params[:scope] ||= DEFAULT_SCOPE
         end
+      end
+
+      # Defines the callback_url "downstream" in stead of more upstream in
+      # the omniauth-oauth2 gem. The newest version of omniauth-oauth2 broke
+      # the previous behavior. More on this, see: https://github.com/intridea/omniauth-oauth2/issues/81
+      # @return [String] the callback_url
+      def callback_url
+        full_host + script_name + callback_path
       end
 
     end
